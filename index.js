d150cc9224251a75f604efe065a41dc709887a38
Adam Mannino <amannino92@gmail.com>
2023-12-16 18:37:14 -0500

    Completes Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki']
+function writeCards(names, eventName) 
+{ let messagesArray = [];
+  for (let i = 0; i < names.length; i++) {
+    messagesArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messagesArray;}
+
+  console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'birthday'))
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
